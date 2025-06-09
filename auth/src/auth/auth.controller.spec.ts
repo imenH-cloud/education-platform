5c5fc8ad23cdd5d7fb2e5c071ac55dd5bc8c82d8
IMEN HAMADA <callcenterimen@gmail.com>
2025-06-09 20:56:54 +0100

    Update auth.controller.spec.ts

--- auth/src/auth/auth.controller.spec.ts
+++ auth/src/auth/auth.controller.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
+  const mockAuthService = {
+    validateUser: jest.fn().mockResolvedValue(true),
+    // Ajoute ici d’autres méthodes mockées si besoin
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
@@ -15,4 +27,6 @@ describe('AuthController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  // Ajoute tes tests ici en utilisant mockAuthService
 });
